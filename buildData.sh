25ef8fab5f6458b32f52e0c611e4676573ec0714
vineeth-mohan <vineethmohan@factweavers.com>
2019-05-11 12:05:19 +0530

    Adding script to sort in right order

--- buildData.sh
+++ buildData.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-sort out | uniq -c | sed 's/^[\t ]*\([0-9][0-9]*\) \(.*\)$/\2,\1/' 
-#| sort -t , -k 1,1 > shorts.txt
+mv nohup.out out
+echo "short form,expansion,occurance" > abbrevations.csv
+sort out | uniq -c | sed 's/^[\t ]*\([0-9][0-9]*\) \(.*\)$/\2,\1/' > unique-abbr.csv
+sort -t, -k 1,1 -k 3,3nr  unique-abbr.csv >> abbrevations.csv
+rm unique-abbr.csv
