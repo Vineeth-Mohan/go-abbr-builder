7f3176e0b2d150e4d74b2dec3a4f250e5050b46e
vineeth-mohan <vineethmohan@factweavers.com>
2019-05-11 17:14:28 +0530

    Adding build script

--- buildData.sh
+++ buildData.sh
@@ -5,3 +5,4 @@ echo "short form,expansion,occurance" > abbrevations.csv
 sort out | uniq -c | sed 's/^[\t ]*\([0-9][0-9]*\) \(.*\)$/\2,\1/' > unique-abbr.csv
 sort -t, -k 1,1 -k 3,3nr  unique-abbr.csv >> abbrevations.csv
 rm unique-abbr.csv
+rm out
